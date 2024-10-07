0fa8414e9bc4c89eb782a15ebf827125f3a79c8d
AKx5x <akx5.developer@gmail.com>
2024-10-07 23:09:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-
+const typingText = document.querySelector('.typing-text span');
+const words = ["Web Developer", "Discord Bot Developer", "Web Designer", "Java Developer", "Minecraft Plugin & Server
